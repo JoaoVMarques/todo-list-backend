a52b3bb975c1a1bdcf28d66418b51e79bbd6276a
JoaoVMarques <J.v.marques.fernandes@gmail.com>
2023-01-22 21:01:27 -0300

    Melhorando o registerSchema

--- src/express/registerSchema.ts
+++ src/express/registerSchema.ts
@@ -1,7 +1,16 @@
 import { body } from 'express-validator';
 
 const registerSchema = [
-  body('email').isEmail(),
+  body('email')
+    .exists()
+    .isEmail(),
+
+  body('username')
+    .exists()
+    .isLength({ max: 20 }),
+
+  body('password')
+    .isLength({ min: 6 }),
 ];
 
 export default registerSchema;
